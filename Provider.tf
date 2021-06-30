35259bca09ce02af80a7861ff849953cdeb55008
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-06-30 18:19:29 -0500

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -10,4 +10,6 @@ terraform{
     provider "aws" { //providing details about the plugin
         profile = "default" // profile changes if there are multiple aws account
         region = "us-east-1"
+        access_key = var.access_key
+        secret_key = var.secret_key 
     }
