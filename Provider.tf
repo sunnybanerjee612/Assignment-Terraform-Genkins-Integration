b35d926aef75cdb5fcf9fd4e9e31628b5e03b4eb
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-06-30 17:53:06 -0500

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,13 @@
+terraform{
+    required_providers { //this is plugin
+      aws = {
+          source = "hashicorp/aws" //location where plugin is located
+          version = "~> 3.27" //best practice is to use version not reqd
+      }
+    }
+}
+
+    provider "aws" { //providing details about the plugin
+        profile = "default" // profile changes if there are multiple aws account
+        region = "us-east-1"
+    }
